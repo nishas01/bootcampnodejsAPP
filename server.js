ccba0481bf1476e7ff6f5d0d69f86e667d03be07
Nisha Singh <84907076+nishas01@users.noreply.github.com>
2026-04-21 17:34:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Rajcosiva V2🚀");
+  res.send("Hello Rajcosiva🚀");
 });
 
 app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
