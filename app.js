4112271646ffece5a291148e78b7fc9fbe46233d
Nisha Singh <84907076+nishas01@users.noreply.github.com>
2026-04-24 17:40:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello from Node.js App running on AKS 🚀");
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
